8fc1336fbc860c23dc540b279a7146c5bfacf64a
Leonardo Silva <leonardoferreira032@gmail.com>
2024-02-23 22:10:50 -0300

    Footer and header structure

--- products/src/app/app.routes.ts
+++ products/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/page/home/home.component';
+import { AboutComponent } from './components/page/about/about.component';
+import { AddProductComponent } from './components/page/add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "product/new",
+        component: AddProductComponent
+    }
+];
