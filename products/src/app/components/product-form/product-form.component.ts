f9c7f71edb72454bc365ca2364c05647a3c2bdc4
Leonardo Silva <leonardoferreira032@gmail.com>
2024-02-23 23:15:00 -0300

    Form validation

--- products/src/app/components/product-form/product-form.component.ts
+++ products/src/app/components/product-form/product-form.component.ts
@@ -1,12 +1,58 @@
-import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  productForm!: FormGroup;
   @Input() btnText!: string;
+
+  ngOnInit(): void {
+    this.productForm = new FormGroup({
+      id: new FormControl(''),
+      nome: new FormControl('', Validators.required),
+      preco: new FormControl('', Validators.required),
+      quantidade: new FormControl('', Validators.required),
+      idFabricante: new FormControl('', Validators.required),
+      idCategoria: new FormControl('', Validators.required),
+    });
+  }
+
+  get nome() {
+    return this.productForm.get('nome')!
+  }
+
+  get preco() {
+    return this.productForm.get('preco')!
+  }
+
+  get quantidade() {
+    return this.productForm.get('quantidade')!
+  }
+
+  get idFabricante() {
+    return this.productForm.get('idFabricante')!
+  }
+
+  get idCategoria() {
+    return this.productForm.get('idCategoria')!
+  }
+
+  submit() {
+    if(this.productForm.invalid) {
+      return
+    }
+
+    console.log("Formulário enviado")
+  }
 }
